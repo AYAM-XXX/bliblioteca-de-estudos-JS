0c68858fb6c17c39c71d65dc7937fc61c182963a
AYAM-XXX <77755711+AYAM-XXX@users.noreply.github.com>
2021-01-25 18:39:30 -0300

    anotações de arrays

---
+++ arrays js/tipos de array.js
@@ -0,0 +1,23 @@
+[]
+.push
+//adiciona elemento a VARIAVEL
+.unshift
+//coloca o elememto em primeiro
+.pop
+// retira o elemento
+.shift
+//remove o primeiro elemento
+.splice
+//remove mais de um elemento 
+.indexOf
+//pesquisa o local do elemento
+.sort
+//ordena os elementos de ordem crescente
+.reverse
+//ordena os elementos de ordem decrecente
+.join
+//coloca os elementos em  forma de string
+.concat
+//junta 2 VARIAVES diferentes
+.lenght
+//informa o valor da VARIAVEL
